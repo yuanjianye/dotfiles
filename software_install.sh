de32c3834501585172c7dbf48082c800b3e9aefa
yuanjianye <yuanjianye@126.com>
2016-07-16 20:17:36 +0800

    mv all files to root dir
    Signed-off-by: yuanjianye <yuanjianye@126.com>

---
+++ software_install.sh
@@ -0,0 +1,4 @@
+apt-get update
+apt-get install git subversion vim tmux zsh i3 nodejs w3m fbterm iceweasel
+apt-get install bison flex gperf zip curl valgrind build-essential texinfo
+apt-get install vsftpd samba openssh 
