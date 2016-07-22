76f8ad5167754f784c9d5450d684b84a53f32383
yuanjianye <yuanjianye@126.com>
2016-07-22 09:12:42 +0800

    cscope_file_gen.sh add dts and dtsi
    Signed-off-by: yuanjianye <yuanjianye@126.com>

--- cscope_file_gen.sh
+++ cscope_file_gen.sh
@@ -1,2 +1,2 @@
-find ./ -name "*.java" -or -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.aidl" -or -name "*.cxx" -or -name "*.cc" >cscope.files
+find ./ -name "*.java" -or -name "*.c" -or -name "*.cpp" -or -name "*.h" -or -name "*.dts" -or -name "*.dtsi" -or -name "*.aidl" -or -name "*.cxx" -or -name "*.cc" >cscope.files
 cscope -bqk -i ./cscope.files
