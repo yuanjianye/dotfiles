3156952a9be62ba2cbe9432373209cebb7320afd
yuanjianye <yuanjianye@126.com>
2016-08-14 09:12:35 -0400

    modified zotero.sh

--- bin/zotero.sh
+++ bin/zotero.sh
@@ -1,4 +1,4 @@
-ZOTERO_PATH=~/.mozilla/firefox/n8vlun98.default/zotero/storage/
+ZOTERO_PATH=$(find $HOME/.mozilla/firefox -maxdepth 2 -name "zotero")/storage
 #ZOTERO_LAST_PATH=`ls $ZOTERO_PATH -t|sed -n '1p'`
 ZOTERO_HTML_FILE=`find $ZOTERO_PATH -size +1000c|grep "htm.$"`
 ZOTERO_HTML_FILE_TIME=`ls -t $ZOTERO_HTML_FILE`
